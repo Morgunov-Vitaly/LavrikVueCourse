18228a9566b77e875644bad966c8eb592bf43337
Morgunov-Vitaly <morgunov.vitaly@gmail.com>
2019-07-13 22:11:11 +0300

    продолжаю разбираться и расставлять комментарии

--- mv-07hw/src/store/modules/cart.js
+++ mv-07hw/src/store/modules/cart.js
@@ -4,7 +4,7 @@ export default {
 		products: []
 	},
 	getters: {
-		inCart: (state) => (id) => state.products.some((el) => el.id === id),
+		inCart: (state) => (id) => state.products.some((el) => el.id === id), //Проверка есть ли товар в корзие
 		total(){
 			//hw
 		}
@@ -23,8 +23,8 @@ export default {
 	actions: {
 		add(store, id){
 			return new Promise(function(resolve, reject){
-				if(!store.getters.inCart(id)){
-					store.commit('add', id);
+				if(!store.getters.inCart(id)){ //проверяем если нет такого товара в корзине
+					store.commit('add', id); //то запускаем одноименную мутацию add с параметром id
 					resolve();
 				}
 				// reject
@@ -32,7 +32,7 @@ export default {
 		},
 		remove(store, id){
 			return new Promise(function(resolve, reject){
-				if(store.getters.inCart(id)){
+				if(store.getters.inCart(id)){ //проверка на наличие данного товара в корзине
 					store.commit('remove', id);
 					resolve();
 				}
